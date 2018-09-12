a5cbe335414eae61595b1e40e932781c080449da
hakogh <cr7.4ever@live.fr>
2018-09-12 20:59:16 +0000

    fixit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - ptint a string
+ * Description - this will print/
+ *"Programming is like building a multilingual puzzle
+ *Return: Returns 0
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
