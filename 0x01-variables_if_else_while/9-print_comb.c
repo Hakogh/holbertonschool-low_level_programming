c6444a59e4ab7a2819f4ce9a16059fc779e5cfac
hakogh <cr7.4ever@live.fr>
2018-09-14 01:52:04 +0000

    print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+int i = 0;
+
+
+
+while (i < 10)
+
+{
+
+putchar(i + '0');
+
+if (i < 9)
+
+{
+
+putchar(44);
+
+putchar(32);
+
+}
+      
+i++;
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
