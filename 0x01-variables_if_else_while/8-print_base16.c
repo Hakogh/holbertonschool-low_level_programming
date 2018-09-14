6ae8f103a7bd33e0e67de60bd5069c00f37914df
hakogh <cr7.4ever@live.fr>
2018-09-14 01:37:25 +0000

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+char c;
+
+int i;
+
+for (i = 0; i < 10; i++)
+
+{
+
+putchar(i + '0');
+
+}
+
+for (c = 'a'; c < 'g'; c++)
+
+{
+
+putchar(c);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
