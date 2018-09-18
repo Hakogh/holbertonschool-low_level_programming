93e17ac77774f63964975b3edd52c1d176ac8c05
hakogh <cr7.4ever@live.fr>
2018-09-18 22:44:28 +0000

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,27 +8,13 @@
 int main(void)
 
 {
-
-  char hb[] = "Holberton";
-
-  int i = 0;
-
-
-
-  while (hb[i] != '\0')
-
-    {
-
-      _putchar(hb[i]);
-
-      i++;
-
-    }
-
-  _putchar('\n');
-
-
-
-  return (0);
-
+char hb[] = "Holberton";
+int i = 0;
+while (hb[i] != '\0')
+{
+_putchar(hb[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
