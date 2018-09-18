f937aba7b3c2d6365ce980e2fc5c89dfe9f06a7d
hakogh <cr7.4ever@live.fr>
2018-09-18 03:59:31 +0000

    alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+
+{
+
+  int i = 0;
+
+
+
+  for (i = 'a'; i <= 'z'; i++)
+
+    {
+
+      _putchar(i);
+
+    }
+
+  _putchar('\n');
+
+}
