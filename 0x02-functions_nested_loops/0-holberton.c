3f829381b8a09f1d7dc244bd5d6dd070b8374328
hakogh <cr7.4ever@live.fr>
2018-09-18 03:43:35 +0000

    hol

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+
+  char hb[] = "Holberton";
+
+  int i = 0;
+
+
+
+  while (hb[i] != '\0')
+
+    {
+
+      _putchar(hb[i]);
+
+      i++;
+
+    }
+
+  _putchar('\n');
+
+
+
+  return (0);
+
+}
