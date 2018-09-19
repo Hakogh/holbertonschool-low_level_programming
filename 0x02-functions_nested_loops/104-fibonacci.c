ea270c88adec47fec52358b9f8548b1f251bad1f
hakogh <cr7.4ever@live.fr>
2018-09-19 17:38:58 +0000

    fab104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,15 +9,15 @@
 int main(void)
 
 {
-unsigned long int first = 1, second = 1, next_num = 1;
-int i = 0, n = 98;
-for (i = 0; i < n; i++)
-{
-printf("%lu, ", next_num);
-first = second;
-second = next_num;
-next_num = first + second;
-}
-printf("\n");
-return (0);
+  unsigned long int first = 1, second = 1, next_num = 1;
+  int i = 0, n = 98;
+  for (i = 0; i < n; i++)
+    {
+      printf("%lu, ", next_num);
+      first = second;
+      second = next_num;
+      next_num = first + second;
+    }
+  printf("\n");
+  return (0);
 }
