b5a0477f50041c431f2bdd7977077ecec8398346
hakogh <cr7.4ever@live.fr>
2018-09-21 03:40:25 +0000

    large

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(void)
+{
+long int num=612852475143;
+int large=0;
+int i;
+
+for(i=2; num!=1; ++i)
+{
+while( num%i==0)
+{
+num/=i;
+large=i;
+}
+}
+printf("%d\n", large);
+return 0;
+}
