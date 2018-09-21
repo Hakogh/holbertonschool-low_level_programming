45fa8514b8d55376369f2080213bc2287632218d
hakogh <cr7.4ever@live.fr>
2018-09-21 16:13:00 +0000

    fxfx

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-long int num = 612852475143;
-int large = 0;
-int i;
-for (i = 2 ; num != 1; ++i)
-{
-while (num % i == 0)
-{
-num /= i;
-large = i;
-}
-}
-printf("%d\n", large);
-return (0);
+  long int num = 612852475143;
+  int large = 0;
+  int i;
+  for (i = 2 ; num != 1; ++i)
+    {
+      while (num % i == 0)
+	{
+	  num /= i;
+	  large = i;
+	}
+    }
+  printf("%d\n", large);
+  return (0);
 }
--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,19 @@
 void print_square(int size)
 
 {
-int x, y;
-y = 0;
-if (size < 1)
-_putchar('\n');
-while (y < size)
-{
-x = 0;
-while (x < size)
-{
-_putchar('#');
-x++;
-}
-_putchar('\n');
-y++;
-}
+  int x, y;
+  y = 0;
+  if (size < 1)
+    _putchar('\n');
+  while (y < size)
+    {
+      x = 0;
+      while (x < size)
+	{
+	  _putchar('#');
+	  x++;
+	}
+      _putchar('\n');
+      y++;
+    }
 }
